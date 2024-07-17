a511bf0a52ed57657ee3faf1d8a769c75a7f278f
Maxence <maxencedek@gmail.com>
2024-07-17 17:23:30 +0200

    mail test

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,6 +6,24 @@ function Contact(){
 return( 
     <section id="ContactLink" className="Contact__Container">
 <HeaderSecond title={'Contact'} Titlecolor={"#212121"} color={'#212121'} NavbarColor={'#212121'} />
+<article>
+
+<form name="contact" method="post" data-netlify="true" onSubmit="submit">
+          
+            <input type="hidden" name="form-name" value="contact" />
+            <label for ="name">Name: </label>
+            <input type="text" name="name"/>
+         
+            <label for="email">Email: </label>
+            <input type="email" name="email"/>
+         
+            <label for="message">Message: </label>
+            <textarea name="message"></textarea>
+          
+            <button type="submit">Send</button>
+
+        </form>
+</article>
     </section>
 )
 
